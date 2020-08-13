11c6cacd07ec9dab9583b584b001f92116443612
TadasGailevicius <tadasgailevicius@yahoo.com>
2020-08-14 00:18:06 +0300

    add Resource.kt

--- app/src/main/java/com/tedm/newsapp/MainActivity.kt
+++ app/src/main/java/com/tedm/newsapp/MainActivity.kt
@@ -2,7 +2,11 @@ package com.tedm.newsapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
+import com.tedm.newsapp.db.ArticleDatabase
+import com.tedm.newsapp.repository.NewsRepository
 import com.tedm.newsapp.ui.NewsViewModel
+import com.tedm.newsapp.ui.NewsViewModelProviderFactory
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,5 +15,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val newsRepository = NewsRepository(ArticleDatabase(this))
+        val viewModelProviderFactory = NewsViewModelProviderFactory(newsRepository)
+        viewModel = ViewModelProvider(this,viewModelProviderFactory).get(NewsViewModel::class.java)
     }
 }
