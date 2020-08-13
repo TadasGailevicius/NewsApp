46be3e859fa7a5536a3aa4697382b1be7c41be50
TadasGailevicius <tadasgailevicius@yahoo.com>
2020-08-13 20:02:44 +0300

    add TypeConverters to ArticleDatabase.kt

--- app/src/main/java/com/tedm/newsapp/db/ArticleDatabase.kt
+++ app/src/main/java/com/tedm/newsapp/db/ArticleDatabase.kt
@@ -4,14 +4,15 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 import com.tedm.newsapp.Article
 import java.security.AccessControlContext
 
 @Database(
     entities = [Article::class],
     version = 1
-
 )
+@TypeConverters(Converters::class)
 abstract class ArticleDatabase : RoomDatabase(){
 
     abstract fun getArticleDao(): ArticleDao
