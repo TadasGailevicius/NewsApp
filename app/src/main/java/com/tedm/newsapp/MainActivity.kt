dfd07aecdae707410d8b4b10aaaad17902915781
TadasGailevicius <tadasgailevicius@yahoo.com>
2020-08-13 23:58:59 +0300

    add NewsViewModelProviderFactory.kt

--- app/src/main/java/com/tedm/newsapp/MainActivity.kt
+++ app/src/main/java/com/tedm/newsapp/MainActivity.kt
@@ -2,8 +2,12 @@ package com.tedm.newsapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.tedm.newsapp.ui.NewsViewModel
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var viewModel: NewsViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
