f85faf1d9644692061a67bc4f70275127f6297ed
TadasGailevicius <tadasgailevicius@yahoo.com>
2020-08-14 22:08:40 +0300

    update NewsRepository.kt update NewsFragment.kt update NewsViewModel.kt

--- app/src/main/java/com/tedm/newsapp/repository/NewsRepository.kt
+++ app/src/main/java/com/tedm/newsapp/repository/NewsRepository.kt
@@ -1,8 +1,11 @@
 package com.tedm.newsapp.repository
 
+import com.tedm.newsapp.api.RetrofitInstance
 import com.tedm.newsapp.db.ArticleDatabase
 
 class NewsRepository(
     val db: ArticleDatabase
 ) {
+    suspend fun getNews(countryCode: String, pageNumber: Int) =
+        RetrofitInstance.api.getNews(countryCode,pageNumber)
 }
--- app/src/main/java/com/tedm/newsapp/ui/fragments/NewsFragment.kt
+++ app/src/main/java/com/tedm/newsapp/ui/fragments/NewsFragment.kt
@@ -3,16 +3,27 @@ package com.tedm.newsapp.ui.fragments
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.tedm.newsapp.MainActivity
 import com.tedm.newsapp.R
+import com.tedm.newsapp.adapters.NewsAdapter
 import com.tedm.newsapp.ui.NewsViewModel
 
 class NewsFragment : Fragment(R.layout.activity_main) {
 
     lateinit var viewModel: NewsViewModel
+    lateinit var newsAdapter: NewsAdapter
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         viewModel = (activity as MainActivity).viewModel
+    }
+
+    private fun setupRecyclerView() {
+        newsAdapter = NewsAdapter()
+        rvBreakingNews.apply {
+            adapter = newsAdapter
+            layoutManager = LinearLayoutManager(activity)
+        }
     }
 }
