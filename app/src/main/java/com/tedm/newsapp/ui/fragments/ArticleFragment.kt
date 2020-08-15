bfdf186f8b6044c4ce81ce2b954b1c3d8dfbad0b
TadasGailevicius <tadasgailevicius@yahoo.com>
2020-08-15 14:18:02 +0300

    Add onViewCreated method to ArticleFragment.kt

--- app/src/main/java/com/tedm/newsapp/ui/fragments/ArticleFragment.kt
+++ app/src/main/java/com/tedm/newsapp/ui/fragments/ArticleFragment.kt
@@ -1,8 +1,27 @@
 package com.tedm.newsapp.ui.fragments
 
+import android.os.Bundle
+import android.view.View
+import android.webkit.WebViewClient
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.navArgs
+import com.tedm.newsapp.MainActivity
 import com.tedm.newsapp.R
+import com.tedm.newsapp.ui.NewsViewModel
+import kotlinx.android.synthetic.main.fragment_article.*
 
 class ArticleFragment : Fragment(R.layout.fragment_article) {
 
+    lateinit var viewModel: NewsViewModel
+    val args: ArticleFragmentArgs by navArgs()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel = (activity as MainActivity).viewModel
+        val article = args.article
+        webView.apply {
+            webViewClient = WebViewClient()
+            loadUrl(article.url)
+        }
+    }
 }
