054c31718358fd3716a2555e455b15cb20456ec8
TadasGailevicius <tadasgailevicius@yahoo.com>
2020-08-15 20:02:44 +0300

    add CollapsingToolbarLayout into fragment_article.xml

--- app/src/main/java/com/tedm/newsapp/ui/fragments/ArticleFragment.kt
+++ app/src/main/java/com/tedm/newsapp/ui/fragments/ArticleFragment.kt
@@ -5,10 +5,13 @@ import android.view.View
 import android.webkit.WebViewClient
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
+import com.bumptech.glide.Glide
 import com.tedm.newsapp.MainActivity
 import com.tedm.newsapp.R
 import com.tedm.newsapp.ui.NewsViewModel
 import kotlinx.android.synthetic.main.fragment_article.*
+import kotlinx.android.synthetic.main.fragment_article_nested_scrollview.*
+import kotlinx.android.synthetic.main.item_article.view.*
 
 class ArticleFragment : Fragment(R.layout.fragment_article) {
 
@@ -20,6 +23,7 @@ class ArticleFragment : Fragment(R.layout.fragment_article) {
         viewModel = (activity as MainActivity).viewModel
         val article = args.article
 
+        Glide.with(this).load(article.urlToImage).into(fragment_article_image)
         fragment_article_title.text = article.title
         fragment_article_description.text = article.description
         fragment_article_author.text = article.author
