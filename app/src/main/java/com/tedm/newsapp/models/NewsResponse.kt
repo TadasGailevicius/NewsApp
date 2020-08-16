9bf2ba541de5a192a89146d1d21d3fd4ba01e804
TadasGailevicius <tadasgailevicius@yahoo.com>
2020-08-16 21:30:46 +0300

    make articles MutableList<Article> in NewsResponse.kt

--- app/src/main/java/com/tedm/newsapp/models/NewsResponse.kt
+++ app/src/main/java/com/tedm/newsapp/models/NewsResponse.kt
@@ -3,7 +3,7 @@ package com.tedm.newsapp.models
 import com.tedm.newsapp.models.Article
 
 data class NewsResponse(
-    val articles: List<Article>,
+    val articles: MutableList<Article>,
     val status: String,
     val totalResults: Int
 )
