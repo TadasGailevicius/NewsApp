e833e7e2379c31f74805f48cc0b4ff3afa96ad72
TadasGailevicius <tadasgailevicius@yahoo.com>
2020-08-16 22:00:07 +0300

    fix NewsFragment.kt

--- app/src/main/java/com/tedm/newsapp/ui/fragments/NewsFragment.kt
+++ app/src/main/java/com/tedm/newsapp/ui/fragments/NewsFragment.kt
@@ -3,14 +3,17 @@ package com.tedm.newsapp.ui.fragments
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.AbsListView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.tedm.newsapp.MainActivity
 import com.tedm.newsapp.R
 import com.tedm.newsapp.adapters.NewsAdapter
 import com.tedm.newsapp.ui.NewsViewModel
+import com.tedm.newsapp.util.Constants.Companion.QUERY_PAGE_SIZE
 import com.tedm.newsapp.util.Resource
 import kotlinx.android.synthetic.main.fragment_news.*
 
@@ -42,7 +45,9 @@ class NewsFragment : Fragment(R.layout.fragment_news) {
                 is Resource.Success -> {
                     hideProgressBar()
                     response.data?.let {newsResponse ->
-                        newsAdapter.differ.submitList(newsResponse.articles)
+                        newsAdapter.differ.submitList(newsResponse.articles.toList())
+                        val totalPages = newsResponse.totalResults / QUERY_PAGE_SIZE + 2
+                        isLastPage = viewModel.newsPage == totalPages
                     }
                 }
                 is Resource.Error -> {
@@ -62,10 +67,47 @@ class NewsFragment : Fragment(R.layout.fragment_news) {
 
     private fun hideProgressBar() {
         paginationProgressBar.visibility = View.INVISIBLE
+        isLoading = false
     }
 
     private fun showProgressBar() {
         paginationProgressBar.visibility = View.VISIBLE
+        isLoading = true
+    }
+
+    var isLoading = false
+    var isLastPage = false
+    var isScrolling = false
+
+    val scrollListener = object : RecyclerView.OnScrollListener() {
+        override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+            super.onScrolled(recyclerView, dx, dy)
+
+            val layoutManager = recyclerView.layoutManager as LinearLayoutManager
+            val firstVisibleItemPosition = layoutManager.findFirstVisibleItemPosition()
+            val visibleItemCount = layoutManager.childCount
+            val totalItemCount = layoutManager.itemCount
+
+            val isNotLoadingAndNotLastPage = !isLoading && !isLastPage
+            val isAtLastItem = firstVisibleItemPosition + visibleItemCount >= totalItemCount
+            val isNotAtBeginning = firstVisibleItemPosition >= 0
+            val isTotalMoreThanVisible = totalItemCount >= QUERY_PAGE_SIZE
+            val shouldPaginate = isNotLoadingAndNotLastPage && isAtLastItem && isNotAtBeginning &&
+                    isTotalMoreThanVisible && isScrolling
+            if(shouldPaginate) {
+                viewModel.getNews("lt")
+                isScrolling = false
+            } else {
+                rvNews.setPadding(0, 0, 0, 0)
+            }
+        }
+
+        override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
+            super.onScrollStateChanged(recyclerView, newState)
+            if(newState == AbsListView.OnScrollListener.SCROLL_STATE_TOUCH_SCROLL) {
+                isScrolling = true
+            }
+        }
     }
 
     private fun setupRecyclerView() {
@@ -73,6 +115,7 @@ class NewsFragment : Fragment(R.layout.fragment_news) {
         rvNews.apply {
             adapter = newsAdapter
             layoutManager = LinearLayoutManager(activity)
+            addOnScrollListener(this@NewsFragment.scrollListener)
         }
     }
 }
